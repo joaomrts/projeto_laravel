2c2e1ef3c537dc515071fc27a8ba2a57993f6b15
joaomrts <joaobatista0811@gmail.com>
2021-06-28 14:40:04 -0300

    autenticao de Usuario

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -53,6 +54,9 @@ class EventController extends Controller
         }
 
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
         return redirect('/')->with('msg', 'Evento criado com sucesso');
     }
@@ -60,6 +64,9 @@ class EventController extends Controller
     public function show($id)
     {
         $event = Event::findOrFail($id);
-        return view('events.show', ['event' => $event]);
+
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
+
+Route::get('/events/create', [EventController::class, 'create'])->middleware('auth');
 Route::get('/', [EventController::class, 'index']);
-Route::get('/events/create', [EventController::class, 'create']);
 Route::get('/events/{id}', [EventController::class, 'show']);
 Route::post('/events', [EventController::class, 'store']);
 
@@ -13,3 +14,7 @@ Route::post('/events', [EventController::class, 'store']);
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
